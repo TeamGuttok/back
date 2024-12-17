0b18e9c5a3886c19e35636f2a5612cc95be1facb
o-tao <dhtkdals369@gmail.com>
2024-12-17 16:51:54 +0900

    Initial commit

---
+++ src/main/java/com/app/guttokback/GuttokBackApplication.java
@@ -0,0 +1,13 @@
+package com.app.guttokback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuttokBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuttokBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/guttokback/GuttokBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.guttokback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuttokBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
