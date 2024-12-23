b70f0763d6cc11ad8bd710577b252a620fe3c321
jucheolkang <jucheol9877@naver.com>
2024-12-24 02:20:20 +0900

    feat : 유저 레포지토리 추가

--- src/main/java/com/app/guttokback/user/repository/UserRepository.java
+++ src/main/java/com/app/guttokback/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.app.guttokback.user.repository;
 
-import com.app.guttokback.user.entity.UserEntity;
+import com.app.guttokback.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
