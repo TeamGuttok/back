99191a39a0993fabf683e036692f72b605acb7bd
jucheolkang <jucheol9877@naver.com>
2024-12-24 15:45:57 +0900

    feat : QueryDSL로 이메일 찾기 추가

--- src/main/java/com/app/guttokback/user/repository/UserRepository.java
+++ src/main/java/com/app/guttokback/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.app.guttokback.user.repository;
 import com.app.guttokback.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
+    Optional<UserEntity> findByEmail(String Email);
 }
