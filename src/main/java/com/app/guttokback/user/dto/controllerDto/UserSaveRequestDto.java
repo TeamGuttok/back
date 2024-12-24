a8da87dc3d1b2ebcd873b447b21df7015a92074a
jucheolkang <jucheol9877@naver.com>
2024-12-25 01:32:06 +0900

    chore : @pattern 방식 사용

--- src/main/java/com/app/guttokback/user/dto/controllerDto/UserSaveRequestDto.java
+++ src/main/java/com/app/guttokback/user/dto/controllerDto/UserSaveRequestDto.java
@@ -1,9 +1,8 @@
 package com.app.guttokback.user.dto.controllerDto;
 
 import com.app.guttokback.user.dto.serviceDto.UserSaveDto;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,15 +12,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSaveRequestDto {
 
-        @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
-        @Size(min = 8, message = "비밀번호는 최소 8자리 이상이어야 합니다.")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{12,}$",
+                message = "비밀번호는 특수문자(@$!%*?&#), 영어 소문자를 포함한 12자리 이상 입력하세요."
+        )
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
         private String password;
 
-        @NotEmpty(message = "이메일은 필수 입력 값입니다.")
-        @Email(message = "유효한 이메일 형식이어야 합니다.")
+        @Pattern(
+                regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+                message = "유효한 이메일 형식이어야 합니다."
+        )
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
         private String email;
 
-        @NotEmpty(message = "닉네임은 필수 입력 값입니다.")
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
         private String nickName;
 
         private boolean alarm;
@@ -42,5 +47,4 @@ public class UserSaveRequestDto {
                         .alarm(this.alarm)
                         .build();
         }
-
 }
