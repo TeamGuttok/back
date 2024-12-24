ac8d6a71e615dc5d6746c8016241fdb2e498bc4c
jucheolkang <jucheol9877@naver.com>
2024-12-24 20:54:30 +0900

    faat : EMAIL_SAME_FOUND 오류 추가

--- src/main/java/com/app/guttokback/global/exception/ErrorCode.java
+++ src/main/java/com/app/guttokback/global/exception/ErrorCode.java
@@ -3,7 +3,8 @@ package com.app.guttokback.global.exception;
 import lombok.Getter;
 
 public enum ErrorCode {
-    EMAIL_NOT_FOUND("해당 EMAIL을 찾을 수 없습니다.");
+    EMAIL_NOT_FOUND("해당 Email을 찾을 수 없습니다."),
+    EMAIL_SAME_FOUND("동일한 Email이 존재합니다");
 
 
     @Getter
