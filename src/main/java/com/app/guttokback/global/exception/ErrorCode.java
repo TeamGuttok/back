3868c3e5116006986aafff6b6bdfd49058e1e37d
jucheolkang <jucheol9877@naver.com>
2024-12-25 19:57:19 +0900

    fix : EMAIL_NOT_FOUND -> PK_NOT_FOUND로 변경

--- src/main/java/com/app/guttokback/global/exception/ErrorCode.java
+++ src/main/java/com/app/guttokback/global/exception/ErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 Email을 찾을 수 없습니다."),
+    PK_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 PK값을 찾을 수 없습니다."),
     EMAIL_SAME_FOUND(HttpStatus.CONFLICT, "동일한 Email이 존재합니다.");
 
     private final HttpStatus httpStatus;
