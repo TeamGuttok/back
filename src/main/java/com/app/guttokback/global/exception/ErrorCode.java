56a5a2eef8757d535f2c9077c3458f0661814798
jucheolkang <jucheol9877@naver.com>
2024-12-26 16:53:56 +0900

    refactor : 에러 응답값 수정

--- src/main/java/com/app/guttokback/global/exception/ErrorCode.java
+++ src/main/java/com/app/guttokback/global/exception/ErrorCode.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    PK_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 PK값을 찾을 수 없습니다."),
-    EMAIL_SAME_FOUND(HttpStatus.CONFLICT, "동일한 Email이 존재합니다.");
+    ID_NOT_FOUND(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다"),
+    EMAIL_SAME_FOUND(HttpStatus.CONFLICT, "중복된 회원이 존재합니다");
 
     private final HttpStatus httpStatus;
     private final String message;
