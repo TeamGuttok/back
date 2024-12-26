e51b18550c00ea8e31cf8b7d6c2ef9a78006eff6
o-tao <dhtkdals369@gmail.com>
2024-12-27 00:31:51 +0900

    chore : ErrorCode 주석 추가

--- src/main/java/com/app/guttokback/global/exception/ErrorCode.java
+++ src/main/java/com/app/guttokback/global/exception/ErrorCode.java
@@ -5,8 +5,11 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
+    // user
     ID_NOT_FOUND(HttpStatus.BAD_REQUEST, "회원을 찾을 수 없습니다"),
     EMAIL_SAME_FOUND(HttpStatus.CONFLICT, "중복된 회원이 존재합니다"),
+
+    // subscription
     SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "구독 서비스를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
