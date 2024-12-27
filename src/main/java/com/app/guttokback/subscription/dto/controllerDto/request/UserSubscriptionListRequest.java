0e3bb38307b8a805107080e550820b7d7f543e21
o-tao <dhtkdals369@gmail.com>
2024-12-28 00:14:45 +0900

    refactor : Dto Setter -> 생성자로 변경

--- src/main/java/com/app/guttokback/subscription/dto/controllerDto/request/UserSubscriptionListRequest.java
+++ src/main/java/com/app/guttokback/subscription/dto/controllerDto/request/UserSubscriptionListRequest.java
@@ -2,11 +2,12 @@ package com.app.guttokback.subscription.dto.controllerDto.request;
 
 import com.app.guttokback.subscription.dto.serviceDto.UserSubscriptionListInfo;
 import jakarta.validation.constraints.Positive;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSubscriptionListRequest {
 
     @Positive(message = "데이터의 id는 양수여야 합니다.")
@@ -14,6 +15,11 @@ public class UserSubscriptionListRequest {
 
     @Positive(message = "한 페이지에 조회 할 데이터 수는 양수여야 합니다.")
     private long size = 10;
+
+    public UserSubscriptionListRequest(Long lastId, Long size) {
+        this.lastId = lastId;
+        this.size = size == null ? 10L : size;
+    }
 
     public UserSubscriptionListInfo toListOption(Long userId) {
         return new UserSubscriptionListInfo(userId, lastId, size);
