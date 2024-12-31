c5569b3638f69e82b12b81803116e69351dd5a2d
jucheolkang <jucheol9877@naver.com>
2024-12-31 13:59:44 +0900

    test : 로그인 성공 테스트 추가

---
+++ src/test/java/com/app/guttokback/user/AuthControllerTest.java
@@ -0,0 +1,67 @@
+package com.app.guttokback.user;
+
+import com.app.guttokback.global.apiResponse.ResponseMessages;
+import com.app.guttokback.global.security.SecurityConfig;
+import com.app.guttokback.user.controller.AuthController;
+import com.app.guttokback.user.dto.controllerDto.LoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
+@Import(SecurityConfig.class) // SecurityConfig를 가져옴
+class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+
+    private final String testEmail = "test@example.com";
+    private final String testPassword = "securePass123!";
+
+    @BeforeEach
+    void setUp() {
+
+        Authentication mockAuthentication = Mockito.mock(Authentication.class);
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(mockAuthentication);
+    }
+
+    @Test
+    void signin_Success() throws Exception {
+        // given
+        LoginRequestDto loginRequestDto = new LoginRequestDto(testEmail, testPassword);
+
+        // when & then
+        mockMvc.perform(post("/api/users/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()) // CSRF 토큰 포함
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andExpect(status().isOk()) // 200 응답 예상
+                .andExpect(jsonPath("$.message").value(ResponseMessages.USER_LOGIN_SUCCESS));
+    }
+
+}
