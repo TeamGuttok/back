c9396633b151d204f0b777ace9e75e1a02bd441c
jucheolkang <jucheol9877@naver.com>
2025-01-13 20:31:31 +0900

    test : logout 테스트 구현

--- src/test/java/com/app/guttokback/user/controller/AuthControllerTest.java
+++ src/test/java/com/app/guttokback/user/controller/AuthControllerTest.java
@@ -3,7 +3,9 @@ package com.app.guttokback.user.controller;
 import com.app.guttokback.global.apiResponse.ResponseMessages;
 import com.app.guttokback.global.security.SecurityConfig;
 import com.app.guttokback.user.dto.controllerDto.LoginRequestDto;
+import com.app.guttokback.user.dto.serviceDto.UserDetailDto;
 import com.app.guttokback.user.repository.UserRepository;
+import com.app.guttokback.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -42,6 +44,9 @@ class AuthControllerTest {
     @MockitoBean
     private UserRepository userRepository;
 
+    @MockitoBean
+    private UserService userService;
+
     private final String testEmail = "test@example.com";
     private final String testPassword = "securePass123!";
 
@@ -51,23 +56,37 @@ class AuthControllerTest {
 
         when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
                 .thenReturn(mockAuthentication);
+
+        when(mockAuthentication.getName()).thenReturn(testEmail);
+
+        when(userService.userDetail(testEmail)).thenReturn(UserDetailDto.builder()
+                .email(testEmail)
+                .nickName("testNick")
+                .alarm(true)
+                .build());
     }
 
     @Test
     @DisplayName("로그인 성공 테스트")
-    void signin_Success() throws Exception {
-        // given
-        LoginRequestDto loginRequestDto = LoginRequestDto.builder()
-                .email(testEmail)
-                .password(testPassword)
-                .build();
+    void signin_success() throws Exception {
+        LoginRequestDto loginRequestDto = new LoginRequestDto(testEmail, testPassword);
 
-        // when & then
         mockMvc.perform(post("/api/users/signin")
-                        .contentType(MediaType.APPLICATION_JSON)
                         .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(loginRequestDto)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value(ResponseMessages.USER_LOGIN_SUCCESS));
+                .andExpect(jsonPath("$.message").value(ResponseMessages.USER_LOGIN_SUCCESS))
+                .andExpect(jsonPath("$.data").value("testNick"));
+    }
+
+    @Test
+    @DisplayName("로그아웃 성공 테스트")
+    void signout_Success() throws Exception {
+        // when & then
+        mockMvc.perform(post("/api/users/signout")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(ResponseMessages.USER_LOGOUT_SUCCESS));
     }
 }
