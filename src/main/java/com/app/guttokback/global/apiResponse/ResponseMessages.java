4dee149108a3ce617c40d2f235cfc959b8f468e3
jucheolkang <jucheol9877@naver.com>
2025-01-17 01:26:19 +0900

    feat : 비밀번호 찾기 인증 성공 추가

--- src/main/java/com/app/guttokback/global/apiResponse/ResponseMessages.java
+++ src/main/java/com/app/guttokback/global/apiResponse/ResponseMessages.java
@@ -8,6 +8,7 @@ public class ResponseMessages {
     public static final String NICKNAME_UPDATE_SUCCESS = "닉네임 수정 성공";
     public static final String ALARM_UPDATE_SUCCESS = "알림 수정 성공";
     public static final String USER_DELETE_SUCCESS = "유저 삭제 성공";
+    public static final String CERTIFICATION_NUMBER_SUCCESS = "비밀번호 찾기 인증 성공";
 
     // userSubscription
     public static final String USER_SUBSCRIPTION_SAVE_SUCCESS = "구독항목 저장 성공";
