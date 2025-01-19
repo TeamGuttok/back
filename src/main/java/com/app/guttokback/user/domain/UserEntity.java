d9b192c473c509e856dc77b174a9be59bcc5aa90
jucheolkang <jucheol9877@naver.com>
2025-01-19 15:09:00 +0900

    feat : user 권한 구현

--- src/main/java/com/app/guttokback/user/domain/UserEntity.java
+++ src/main/java/com/app/guttokback/user/domain/UserEntity.java
@@ -1,8 +1,15 @@
 package com.app.guttokback.user.domain;
 
 import com.app.guttokback.global.jpa.AuditInformation;
+import com.app.guttokback.global.security.role.Roles;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,8 +21,10 @@ import org.hibernate.annotations.SQLRestriction;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Getter
@@ -45,12 +54,20 @@ public class UserEntity extends AuditInformation implements UserDetails {
     @Comment("논리적삭제")
     private boolean delete;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "USER_ROLES", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @Comment("사용자 권한")
+    private List<Roles> roles = new ArrayList<>();
+
     @Builder
-    public UserEntity(String password, String email, String nickName, boolean alarm) {
+    public UserEntity(String password, String email, String nickName, boolean alarm, Roles role) {
         this.password = password;
         this.email = email;
         this.nickName = nickName;
         this.alarm = alarm;
+        this.roles.add(role);
     }
 
     public void passwordChange(String password) {
