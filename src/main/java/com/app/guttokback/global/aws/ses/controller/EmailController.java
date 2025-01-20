f6049a7422c4efe0bb1953cf45582e209865901e
o-tao <dhtkdals369@gmail.com>
2025-01-20 18:37:55 +0900

    chore : 임시 컨트롤러 제거, find-password 클래스명, 엔드포인트 변경

--- src/main/java/com/app/guttokback/global/aws/ses/controller/EmailController.java
+++ src/main/java/com/app/guttokback/global/aws/ses/controller/EmailController.java
@@ -1,27 +1,29 @@
 package com.app.guttokback.global.aws.ses.controller;
 
 import com.app.guttokback.global.apiResponse.ApiResponse;
-import com.app.guttokback.global.aws.ses.service.RemainderService;
+import com.app.guttokback.global.aws.ses.dto.controllerDto.UserEmailRequestDto;
+import com.app.guttokback.global.aws.ses.service.CertificationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.app.guttokback.global.apiResponse.ResponseMessages.REMINDER_EMAIL_SEND_SUCCESS;
+import static com.app.guttokback.global.apiResponse.ResponseMessages.CERTIFICATION_EMAIL_SEND_SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mail")
 public class EmailController {
 
-    private final RemainderService remainderService;
+    private final CertificationService certificationService;
 
-    @PostMapping("/reminder")
-    public ResponseEntity<ApiResponse<String>> sendReminder() {
-        remainderService.sendReminder();
-        return ApiResponse.success(REMINDER_EMAIL_SEND_SUCCESS);
+    @PostMapping("/certification")
+    public ResponseEntity<ApiResponse<Object>> userFindPassword(@Valid @RequestBody UserEmailRequestDto userEmailRequestDto) {
+        certificationService.sendCertificationNumber(userEmailRequestDto.getEmailDto());
+        return ApiResponse.success(CERTIFICATION_EMAIL_SEND_SUCCESS);
     }
+    
 }
-
-// 테스트를 위한 임시 컨트롤러 → 스케줄러 적용 후 제거
