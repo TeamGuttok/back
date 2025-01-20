e0a7e00592554887a5a18b2c679da788b2ead976
o-tao <dhtkdals369@gmail.com>
2025-01-20 18:40:54 +0900

    feat : Spring Scheduler 적용

---
+++ src/main/java/com/app/guttokback/global/scheduler/SchedulerTask.java
@@ -0,0 +1,23 @@
+package com.app.guttokback.global.scheduler;
+
+import com.app.guttokback.global.aws.ses.service.ReminderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SchedulerTask {
+
+    private final ReminderService reminderService;
+
+//    @Scheduled(zone = "Asia/Seoul", cron = "0 0 9 * * ?") // 타임존 서울, 매일 09시 실행
+//    public void sendReminder() {
+//        reminderService.sendReminder(LocalDate.now());
+//    }
+
+    /*
+     * 애플리케이션 실행 시 주기적인 동작으로 임시 주석처리
+     * 테스트 시 주석 해제 후 애플리케이션 실행
+     * */
+
+}
