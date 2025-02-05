3dc4c7a3fd82e774aa05dd6c5c8fb56bf4485af1
o-tao <dhtkdals369@gmail.com>
2025-02-05 16:08:47 +0900

    test : 알림 상태 변경 컨트롤러 테스트 코드 작성

--- src/test/java/com/app/guttokback/notification/controller/NotificationControllerTest.java
+++ src/test/java/com/app/guttokback/notification/controller/NotificationControllerTest.java
@@ -1,6 +1,7 @@
 package com.app.guttokback.notification.controller;
 
 import com.app.guttokback.global.apiResponse.PageResponse;
+import com.app.guttokback.global.apiResponse.ResponseMessages;
 import com.app.guttokback.global.apiResponse.util.PageOption;
 import com.app.guttokback.global.apiResponse.util.PageRequest;
 import com.app.guttokback.notification.domain.Category;
@@ -8,6 +9,7 @@ import com.app.guttokback.notification.domain.Status;
 import com.app.guttokback.notification.dto.controllerDto.response.NotificationListResponse;
 import com.app.guttokback.notification.service.NotificationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,6 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,8 +41,10 @@ class NotificationControllerTest {
     private NotificationService notificationService;
 
     private final Long testId = 1L;
+    private final String testEmail = "test@test.com";
 
     @Test
+    @DisplayName("회원에 대해 생성된 알림 조회 시 정상적으로 응답된다.")
     public void NotificationListTest() throws Exception {
         // given
         PageRequest pageRequest = new PageRequest(testId, 1L);
@@ -65,6 +70,19 @@ class NotificationControllerTest {
                 .andExpect(jsonPath("$.hasNext").value(false));
 
         verify(notificationService).list(any(PageOption.class));
+    }
+
+    @Test
+    @WithMockUser(username = "test@test.com")
+    @DisplayName("회원에 대해 생성된 알림 읽음 업데이트 시 성공 응답을 반환한다.")
+    public void notificationUpdateTest() throws Exception {
+        mockMvc.perform(put("/api/notifications")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(ResponseMessages.NOTIFICATION_READ_SUCCESS));
+
+        verify(notificationService).statusUpdate(testEmail);
     }
 
 }
