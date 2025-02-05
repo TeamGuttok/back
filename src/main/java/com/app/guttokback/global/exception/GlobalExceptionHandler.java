3c2fa50f36c7ecc5ef6abc3406c12858f033c9a9
o-tao <dhtkdals369@gmail.com>
2025-02-05 15:10:46 +0900

    chore : ExceptionHandler log 추가

--- src/main/java/com/app/guttokback/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/guttokback/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.app.guttokback.global.exception;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -11,12 +12,14 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.Objects;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<CustomExceptionResponse> responseBindException(BindException exception) {
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+        log.info(exception.getMessage(), exception);
         return new ResponseEntity<>(
                 new CustomExceptionResponse(httpStatus, "BAD_REQUEST_ERROR", Objects.requireNonNull(exception.getFieldError()).getDefaultMessage()),
                 httpStatus
@@ -30,6 +33,7 @@ public class GlobalExceptionHandler {
                 exception.getErrorCode().getErrorCode(),
                 exception.getErrorCode().getMessage()
         );
+        log.info(exception.getMessage(), exception);
         return new ResponseEntity<>(errorResponse, exception.getErrorCode().getHttpStatus());
     }
 
@@ -42,21 +46,24 @@ public class GlobalExceptionHandler {
         String errorMessage = String.format(ErrorCode.INVALID_JSON_INPUT.getMessage(), fieldName, targetType);
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
 
+        log.info(errorMessage, exception);
         return new ResponseEntity<>(
                 new CustomExceptionResponse(httpStatus, ErrorCode.INVALID_JSON_INPUT.getErrorCode(), Objects.requireNonNull(errorMessage)), httpStatus);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<CustomExceptionResponse> responseNoResourceFoundException() {
+    public ResponseEntity<CustomExceptionResponse> responseNoResourceFoundException(NoResourceFoundException exception) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
         CustomExceptionResponse errorResult = new CustomExceptionResponse(httpStatus, "ENDPOINT_ERROR", "잘못된 요청입니다.");
+        log.info(exception.getMessage(), exception);
         return new ResponseEntity<>(errorResult, httpStatus);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CustomExceptionResponse> responseException() {
+    public ResponseEntity<CustomExceptionResponse> responseException(Exception exception) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         CustomExceptionResponse errorResponse = new CustomExceptionResponse(httpStatus, "SERVER_ERROR", "서버 내부 오류가 발생했습니다.");
+        log.error(exception.getMessage(), exception);
         return new ResponseEntity<>(errorResponse, httpStatus);
     }
 }
