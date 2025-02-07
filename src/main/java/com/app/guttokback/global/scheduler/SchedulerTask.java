49f04a8e0695d32d3f7f63b4a59c56dcb2259f3f
o-tao <dhtkdals369@gmail.com>
2025-02-08 02:21:35 +0900

    feat : Scheduler Shedlock 적용

--- src/main/java/com/app/guttokback/global/scheduler/SchedulerTask.java
+++ src/main/java/com/app/guttokback/global/scheduler/SchedulerTask.java
@@ -2,19 +2,22 @@ package com.app.guttokback.global.scheduler;
 
 import com.app.guttokback.global.aws.ses.service.ReminderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SchedulerTask {
 
     private final ReminderService reminderService;
-
-//    @Scheduled(zone = "Asia/Seoul", cron = "0 0 9 * * ?") // 타임존 서울, 매일 09시 실행
-//    public void sendReminder() {
-//        reminderService.sendReminder(LocalDate.now());
-//    }
-
+/*
+    @Scheduled(zone = "Asia/Seoul", cron = "0 0 9 * * ?") // 타임존 서울, 매일 09시 실행
+    @SchedulerLock(name = "test", lockAtMostFor = "1h", lockAtLeastFor = "5m")
+    public void sendReminder() {
+        reminderService.sendReminder(LocalDate.now());
+    }
+*/
     /*
      * 애플리케이션 실행 시 주기적인 동작으로 임시 주석처리
      * 테스트 시 주석 해제 후 애플리케이션 실행
